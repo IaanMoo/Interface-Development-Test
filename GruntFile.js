e1f1d7abd06ad4903a6e48ad3e486b5efd922a22
ianetruiste <ian.etruiste@achievewithoutborders.com>
2024-03-07 23:13:57 +0800

    Added favicon, updated title of the html file and updated and used Grunt build tool

--- GruntFile.js
+++ GruntFile.js
@@ -1,25 +1,40 @@
 module.exports = function(grunt) {
+    // Project configuration
     grunt.initConfig({
+        // Configure tasks
         cssmin: {
             target: {
                 files: {
-                    'dist/styles.min.css': ['styles.css']
+                    'dist/style.min.css': ['src/style.css']
                 }
             }
         },
         uglify: {
             target: {
                 files: {
-                    'dist/script.min.js': ['script.js']
+                    'dist/script.min.js': ['src/script.js']
                 }
             }
-        }
+        },
+        htmlmin: {
+            options: {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeRedundantAttributes: true,
+              minifyJS: true,
+              minifyCSS: true
+            },
+            files: {
+              'dist/index.html': 'src/index.html'
+            }
+          }
     });
 
-    // Load plugins
+    // Load Grunt plugins
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
     // Register tasks
-    grunt.registerTask('default', ['cssmin', 'uglify']);
+    grunt.registerTask('default', ['cssmin', 'uglify', 'htmlmin']);
 };
